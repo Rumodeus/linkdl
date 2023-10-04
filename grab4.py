a300d376f5b1f45c185783505001eb9c728f9a7e
Rikka <rikkahidaka@gmail.com>
2023-10-05 00:10:04 +0100

    error handling

--- grab4.py
+++ grab4.py
@@ -1,6 +1,6 @@
 from io import BytesIO
+from flask import Flask, render_template, request, send_file, jsonify, make_response
 
-from flask import Flask, render_template, request, send_file, jsonify
 from pytube import YouTube
 
 app = Flask(__name__)
@@ -22,38 +22,42 @@ def generate_embed():
 
 @app.route('/')
 def index():
-  return render_template('index.html')
-
+    return render_template('index.html')
 
 @app.route('/download', methods=['GET'])
 def download_file():
-  if request.method == 'GET':
-    video_url = request.args.get('link')
-    format_choice = request.args.get('format')
+    if request.method == 'GET':
+        try:
+            video_url = request.args.get('link')
+            format_choice = request.args.get('format')
 
-    yt = YouTube(video_url)
+            yt = YouTube(video_url)
 
-    if format_choice == 'mp4':
-      stream = yt.streams.get_highest_resolution()
-      video_data = BytesIO()
-      stream.stream_to_buffer(video_data)
-      video_data.seek(0)
+            if format_choice == 'mp4':
+                stream = yt.streams.get_highest_resolution()
+                video_data = BytesIO()
+                stream.stream_to_buffer(video_data)
+                video_data.seek(0)
 
-      return send_file(video_data,
-                       as_attachment=True,
-                       download_name=f'{yt.title}.mp4',
-                       mimetype='video/mp4')
-    elif format_choice == 'mp3':
-      audio_stream = yt.streams.filter(only_audio=True).first()
-      audio_data = BytesIO()
-      audio_stream.stream_to_buffer(audio_data)
-      audio_data.seek(0)
+                return send_file(video_data,
+                                as_attachment=True,
+                                download_name=f'{yt.title}.mp4',
+                                mimetype='video/mp4')
+            elif format_choice == 'mp3':
+                audio_stream = yt.streams.filter(only_audio=True).first()
+                audio_data = BytesIO()
+                audio_stream.stream_to_buffer(audio_data)
+                audio_data.seek(0)
 
-      return send_file(audio_data,
-                       as_attachment=True,
-                       download_name=f'{yt.title}.mp3',
-                       mimetype='audio/mp3')
+                return send_file(audio_data,
+                                as_attachment=True,
+                                download_name=f'{yt.title}.mp3',
+                                mimetype='audio/mp3')
+        except Exception as e:
+            error_code = 500 
+            error_image_url = "https://files.catbox.moe/eq311s.png"
+            return render_template('error.html', error_code=error_code, error_image_url=error_image_url)
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
