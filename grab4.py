6542fa7908c4b393e31700ba01acd8f40d8a5141
Rikka <rikkahidaka@gmail.com>
2023-10-05 23:22:08 +0100

    added progress bar
    god bless chatgpt

--- grab4.py
+++ grab4.py
@@ -1,24 +1,42 @@
-from io import BytesIO
-from flask import Flask, render_template, request, send_file, jsonify, make_response
-
+from flask import Flask, render_template, request, send_file, jsonify, session, Response
 from pytube import YouTube
+import requests
+from tqdm import tqdm
+from io import BytesIO
+import secrets
 
 app = Flask(__name__)
 
-@app.route('/embed', methods=['GET'])
-def generate_embed():
-    embed_data = {
-        "embeds": [
-            {
-                "title": "Rikka's Youtube Downloader",
-                "description": "hiiiiiiii",
-                "image": {
-                    "url": "https://files.catbox.moe/ahrytx.png"
-                }
-            }
-        ]
-    }
-    return jsonify(embed_data)
+# Secret key for session management
+app.secret_key = secret_key = secrets.token_hex(32)
+
+# Variable to track download progress
+download_in_progress = False
+download_progress = 0
+
+# Function to download a video with a progress bar
+def download_video_with_progress(url, format_choice):
+    global download_progress  # Declare the variable as global
+    download_progress = 0  # Set download progress to 0 at the start
+
+    yt = YouTube(url)
+    
+    if format_choice == 'mp4':
+        stream = yt.streams.get_highest_resolution()
+        total_bytes = stream.filesize  # Get the total file size in bytes
+        video_data = BytesIO()
+        
+        response = requests.get(stream.url, stream=True)
+        with tqdm(total=total_bytes, unit='B', unit_scale=True) as pbar:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    video_data.write(chunk)
+                    pbar.update(len(chunk))
+                    download_progress = (video_data.tell() / total_bytes) * 100  # Update download progress
+        
+        video_data.seek(0)
+        return video_data, f'{yt.title}.mp4', 'video/mp4', total_bytes
+
 
 @app.route('/')
 def index():
@@ -26,38 +44,35 @@ def index():
 
 @app.route('/download', methods=['GET'])
 def download_file():
+    global download_in_progress
+
     if request.method == 'GET':
         try:
             video_url = request.args.get('link')
             format_choice = request.args.get('format')
 
-            yt = YouTube(video_url)
+            video_data, filename, mimetype, total_bytes = download_video_with_progress(video_url, format_choice)
 
-            if format_choice == 'mp4':
-                stream = yt.streams.get_highest_resolution()
-                video_data = BytesIO()
-                stream.stream_to_buffer(video_data)
-                video_data.seek(0)
+            def generate():
+                with tqdm(total=total_bytes, unit='B', unit_scale=True) as pbar:
+                    for chunk in video_data:
+                        yield chunk
+                        pbar.update(len(chunk))
 
-                return send_file(video_data,
-                                as_attachment=True,
-                                download_name=f'{yt.title}.mp4',
-                                mimetype='video/mp4')
-            elif format_choice == 'mp3':
-                audio_stream = yt.streams.filter(only_audio=True).first()
-                audio_data = BytesIO()
-                audio_stream.stream_to_buffer(audio_data)
-                audio_data.seek(0)
-
-                return send_file(audio_data,
-                                as_attachment=True,
-                                download_name=f'{yt.title}.mp3',
-                                mimetype='audio/mp3')
+            download_in_progress = False
+            return Response(generate(), content_type=mimetype, headers={"Content-Disposition": f"attachment; filename={filename}"})
         except Exception as e:
-            error_code = 500 
+            error_code = str(e)
+            if error_code == "500":
+                error_code == "500 (you probably entered an invalid link)"
             error_image_url = "https://files.catbox.moe/eq311s.png"
             return render_template('error.html', error_code=error_code, error_image_url=error_image_url)
 
+@app.route('/progress', methods=['GET'])
+def get_progress():
+    global download_progress  # Declare the variable as global
+    return jsonify({"progress": download_progress})
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
